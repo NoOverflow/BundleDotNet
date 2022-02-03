469a01d0f9ec606d809b85ed635a870a3444c7fa
NoOverflow <julien.prost@epitech.eu>
2022-02-04 00:51:04 +0100

    Add QrLibTester helper project

---
+++ QrLibTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Graphics;
+using QrLib;
+using System;
+using System.Drawing;
+
+namespace QrLibTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IQrGenerator qrGenerator = new QrGenerator();
+            byte[] imgData = qrGenerator.FromText("Hello!");
+
+            using (FileStream sw = new FileStream("test_qr.png", FileMode.OpenOrCreate))
+            {
+                sw.Write(imgData);
+            }
+            Console.WriteLine("Done! Check /bin/Debug/net6.0/test_qr.png");
+        }
+    }
+}
